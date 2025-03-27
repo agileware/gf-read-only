6748d544afbee2fd8f865a85ae72aff554a2543a
Justin Freeman <justin@agileware.com.au>
2025-03-27 15:46:19 +1100

    Fix WP plugin meta

--- gf-read-only.php
+++ gf-read-only.php
@@ -1,12 +1,13 @@
 <?php
 /**
- *
- * Plugin Name:  Gravity Forms Read Only
- * Plugin URI:   https://github.com/agileware/gf-read-only
- * Description:  Gravity Forms plugin that disables input fields which have a .gf_readonly CSS class. Based on https://docs.gravityforms.com/making-a-field-read-only/
- * Author:       Agileware
- * Version:      0.1
- * Author URI:   https://agileware.com.au
+ * Plugin Name: Gravity Forms Read Only
+ * Plugin URI: https://github.com/agileware/gf-read-only
+ * Description: Gravity Forms plugin that disables input fields which have a .gf_readonly CSS class. Based on https://docs.gravityforms.com/making-a-field-read-only/
+ * Author: Agileware
+ * Version: 0.1
+ * Author URI: https://agileware.com.au
+ * Text Domain: gf-read-only
+ * Domain Path: /languages
  *
  */
 
@@ -22,3 +23,4 @@ add_filter( 'gform_pre_render', function( $form ) {
     return $form;
   }
 );
+
